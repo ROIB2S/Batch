10254b2a66d60c87546005172a46905ba76d90e0
Grant Moyle <Grant.A.Moyle@roitraining.com>
2022-12-05 09:04:25 -0500

    Create beam_demo.py

---
+++ beam_demo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import apache_beam as beam
+
+def processline(line):
+    yield line
+
+def run():
+    argv = [
+    ]
+
+    pipeline = beam.Pipeline(argv=argv)
+    input = 'samples\*.csv'
+    output = 'output\output'
+    
+    (pipeline
+     | 'Read Files' >> beam.io.ReadFromText(input)
+     | 'Process Lines' >> beam.FlatMap(lambda line: processline(line))
+     | 'Write Output' >> beam.io.WriteToText(output)
+     )
+    pipeline.run()
+
+
+if __name__ == '__main__':
+    run()
